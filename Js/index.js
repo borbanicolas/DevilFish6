47fa15346b0cbb8d09e60873795598bcfd271430
Borba <borbanicolasmk@gmail.com>
2022-03-20 16:14:33 -0300

    trabalhando no filtro da tabela de produtos

--- Js/index.js
+++ Js/index.js
@@ -1,31 +1,15 @@
-import { Carrinho } from "./carrinho.js";
-import { Cliente } from "./cliente.js";
-import { Produto } from "./Produto.js";
+const mudarTelaRelease = document.querySelector('[data-release-btn]')
+const mudarTelaShop = document.querySelector('[data-shop-btn]')
+const frame = document.querySelector('[data-frame]')
 
-
-const clienteNicolas =  new Cliente("Nicolas","clienteNicolas", 5555)
-
-
-
-const camiseta = new Produto("camiseta", "roupa", "vermelho", "M", 10, 1)
-
-const calca = new Produto("calca", "roupa", "azul", "calca jeans", 75, 1)
-
-const jaqueta = new Produto("jaqueta", "roupa", "preto", "jaqueta jeans preta", 110, 1)
-
-
-
-
-const carrinho1 = new Carrinho()
-carrinho1.cliente = \
-carrinho1.addProduto(camiseta)
-carrinho1.addProduto(calca)
-carrinho1.addProduto(jaqueta)
-
-carrinho1.removerProduto(calca)
-carrinho1.alterarQuantidadeNoCarrinho(jaqueta, 300 )
-carrinho1.alterarQuantidadeNoCarrinho(camiseta, 20)
-
-console.log(carrinho1)
-
-
+//navigationFrame
+mudarTelaRelease.addEventListener('click',(e) => {
+    e.preventDefault()
+    frame.classList.toggle('active')
+    frame.src = "release.html"
+})
+mudarTelaShop.addEventListener('click',(e) => {
+    e.preventDefault()
+    frame.classList.toggle('active')
+    frame.src = "shop.html"
+})
